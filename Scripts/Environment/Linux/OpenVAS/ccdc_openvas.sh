3e11ad1c80866dc9787e730576c2344cc7c393d5
Mike Crawford <dev.random.87@gmail.com>
2023-11-21 14:55:01 -0800

    Template

--- Scripts/Environment/Linux/OpenVAS/ccdc_openvas.sh
+++ Scripts/Environment/Linux/OpenVAS/ccdc_openvas.sh
@@ -1,3 +1,62 @@
 #!/bin/bash
 
 # Display help menu with -h or --help
+display_help() {
+    echo "Usage: $0 [option...] {download|install|configure}"
+    echo
+    echo "   -h, --help                 Show this help message"
+    echo "   -d, --download             Download OpenVAS"
+    echo "   -i, --install              Install OpenVAS"
+    echo "   -c, --configure            Configure OpenVAS"
+    echo
+    echo "Example: $0 --download"
+    echo
+    exit 1
+}
+
+# Check for root
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root"
+        exit 1
+    fi
+}
+
+# Check for empty arguments
+if [ -z "$1" ]; then
+    display_help
+fi
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+    -h | --help)
+        display_help
+        ;;
+    -d | --download)
+        check_root
+        download_openvas
+        shift
+        ;;
+    -i | --install)
+        check_root
+        install_openvas
+        shift
+        ;;
+    -c | --configure)
+        check_root
+        configure_openvas
+        shift
+        ;;
+    *)
+        display_help
+        ;;
+    esac
+done
+
+main() {
+    check_root
+}
+
+main "$@"
