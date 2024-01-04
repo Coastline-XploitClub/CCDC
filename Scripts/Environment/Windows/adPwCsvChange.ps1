2722f76547a741f4338383ad2894509faec51931
Mike Crawford <dev.random.87@gmail.com>
2024-01-04 14:31:33 -0800

    Proposing update to include numbers, special characters, and caps

--- Scripts/Environment/Windows/adPwCsvChange.ps1
+++ Scripts/Environment/Windows/adPwCsvChange.ps1
@@ -1,10 +1,21 @@
 $ADusers = Get-ADUser -Filter * | Where-Object {$_.SamAccountName -ne "Administrator"} 
 $passlength= 10
-$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-={}|[]\;'.ToCharArray()
+$chars = 'abcdefghijklmnopqrstuvwxyz'.ToCharArray()
+$specialChars = '!@#$%^&*()_+-={}|[]\;'.ToCharArray()
+$upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+$numbers = '0123456789'.ToCharArray()
 
 $userObjects = @()
 foreach ($user in $ADusers){
-    $randompass = -join ($chars | Get-Random -Count $passlength)
+    # Add a random special character, uppercase character, and number to the password
+    $randompass = -join (($specialChars | Get-Random -Count 1), ($upperCaseChars | Get-Random -Count 1), ($numbers | Get-Random -Count 1))
+    
+    # Add random characters from the $chars array to the password until it reaches the $passlength
+    $randompass += -join ($chars.ToCharArray() | Get-Random -Count ($passlength - 3))
+
+    # Shuffle the password
+    $randompass = -join ($randompass.ToCharArray() | Get-Random -Count $randompass.Length)
+
     $userObject = New-Object PSObject -Property @{
         SamAccountName = $user.SamAccountName
         Password = "$randompass"
