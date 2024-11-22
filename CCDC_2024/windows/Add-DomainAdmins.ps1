1ca718064e68c4843e2b17b217744b2cc8d7f504
Cameron Cisneros <85032657+cactus-dad@users.noreply.github.com>
2024-11-21 22:15:41 -0800

    Create Add-DomainAdmins.ps1

---
+++ CCDC_2024/windows/Add-DomainAdmins.ps1
@@ -0,0 +1,21 @@
+# create domain admins for each windows team member...remember to change your password!
+$password = ConvertTo-SecureString -AsPlainText -Force '#!C0@stCCDCteam!' 
+
+# Array of names
+$Names = @('kyle','ceasar', 'cam', 'marshall')
+
+foreach ($name in $Names) {
+    try {
+        # Create AD User
+        $samacct = "${name}_da"
+        New-ADUser -Enabled $true -Name "$samacct" -AccountPassword $password -SamAccountName $samacct -Description "${name}'s DA account" -ErrorAction Stop
+
+        # Add to Domain Admins group
+        Add-ADGroupMember -Identity 'Domain Admins' -Members $samacct -ErrorAction Stop
+
+        Write-Output "User ${samacct} created and added to Domain Admins successfully."
+    }
+    catch {
+        Write-Output "Failed to create user ${samacct}: $_"
+    }
+}
