e0bbe0d105b9861d2fa97f929124a1ee4967c398
Cesar Luque <cesarluque2000@gmail.com>
2024-12-04 22:58:34 -0800

    Update New-LocalAdmins.ps1

--- CCDC_2024/windows/New-LocalAdmins.ps1
+++ CCDC_2024/windows/New-LocalAdmins.ps1
@@ -1,2 +1,2 @@
-$Names = @('ceasar_la', 'cam_la', 'marshall_la')
+$Names = @('cesar_la', 'cam_la', 'marshall_la')
 foreach ($name in $Names){net user /add /Y $name '#!C0@stCCDCteam!';net localgroup /add /Y 'Administrators' $name}
