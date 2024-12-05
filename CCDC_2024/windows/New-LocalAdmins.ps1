ffcdebe12bab6e1e6b55d978bde5d22c67adf6ea
Cesar Luque <cesarluque2000@gmail.com>
2024-12-05 10:28:34 -0800

    Update New-LocalAdmins.ps1

--- CCDC_2024/windows/New-LocalAdmins.ps1
+++ CCDC_2024/windows/New-LocalAdmins.ps1
@@ -1,2 +1,2 @@
 $Names = @('cesar_la', 'cam_la', 'marshall_la')
-foreach ($name in $Names){net user /add /Y $name '#!C0@stCCDCteam!';net localgroup /add /Y 'Administrators' $name}
+foreach ($name in $Names){net user /add /Y $name '#!C0@stCCDCteam!';net localgroup /add /Y 'Administrators' $name;net localgroup /add /Y 'Remote Desktop Users' $name}
